31a22bddebac1a98d61b292ddfbf019732faa8bd
Dimitrios Lowe <dimitrioslowe@gmail.com>
2025-06-09 09:48:27 -0400

    got through what I could

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import tvShows from "./shows/data.js";
+import { useState } from "react";
+
 export default function App() {
+  //console.log(tvShows);
+  const [show, setShow] = useState(tvShows);
+
   return (
     <>
       <header>
         <p>React TV</p>
       </header>
       <main></main>
+
+      {show.map((individualShow) => {
+        return (
+          <li>
+            <h2>{individualShow.name}</h2>
+
+            <p>{individualShow.genre}</p>
+          </li>
+        );
+      })}
     </>
   );
 }
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,7 @@
+import tvShows from "./data.js";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
+export default function ShowDetails(show) {
   return <div className="show-details"></div>;
 }
